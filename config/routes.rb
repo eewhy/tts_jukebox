dc33b6179534385c87b4d8a29513e421b1b417de
ey <eyy0221@gmail.com>
2018-08-13 20:06:33 -0400

    delete, edits, save

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/songs" => "songs#index", as: :songs
-  get "/song/new" => "songs#new", as: :new_song
+#  get "/songs" => "songs#index", as: :songs
+#  get "/songs/new" => "songs#new", as: :new_song
+#  get "/songs/:id" => "songs#edit", as: :edit_song
+#
+#  post "/songs" => "songs#create"
+#  put "/songs"
+#
+#  delete "/songs/:id" => "songs#destroy", as: :destroy_song
+# http://mywebsite/songs/12388923847 :id translate to number
 
-  post "/songs" => "songs#create"
+  resources :songs
 end
